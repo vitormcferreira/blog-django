6dde9089612fde232a35baa691ed7683aa518871
Vitor <>
2023-01-04 20:58:32 -0300

    adicionar views para CRUD de Post

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,17 @@
+from django.urls import path
+from django.views import generic
+
+from . import models, views
+
 app_name = 'blog'
 
-urlpatterns = []
+urlpatterns = [
+    path('', generic.ListView.as_view(
+        model=models.Post,
+        template_name='blog/home.html',
+    ), name='home'),
+    path('post_create/', views.PostCreateView.as_view(), name='post_create'),
+    path('post_update/<pk>/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post_delete/<pk>/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post_detail/<pk>/', views.PostDetailView.as_view(), name='post_detail'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from . import models
+
+
+class PostViewMixin:
+    model = models.Post
+    fields = ['title', 'abstract', 'text']
+
+
+class SuccessUrlToPostDetailMixin:
+    def get_success_url(self) -> str:
+        return reverse_lazy('blog:post_detail', args=[self.object.pk])
+
+
+class PostCreateView(
+    PostViewMixin,
+    LoginRequiredMixin,
+    SuccessUrlToPostDetailMixin,
+    generic.CreateView
+):
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+
+        self.object.author = self.request.user
+        self.object.save()
+
+        return redirect(self.get_success_url())
+
+
+class PostUpdateView(
+    PostViewMixin,
+    LoginRequiredMixin,
+    SuccessUrlToPostDetailMixin,
+    generic.UpdateView
+):
+    pass
+
+
+class PostDeleteView(
+    PostViewMixin,
+    LoginRequiredMixin,
+    generic.DeleteView
+):
+    success_url = reverse_lazy('blog:home')
+
+
+class PostDetailView(PostViewMixin, generic.DetailView):
+    pass
